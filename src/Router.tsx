78af7dfb0ebb36403ab21108cdee38f8c13245b1
gabs <gabrielazbarreto@gmail.com>
2025-05-28 19:00:54 -0300

    add default layout for header component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Cart } from "./pages/Cart"
-//import { DefaultLayout } from "./layout/DefaultLayout"
+import { DefaultLayout } from "./layout/DefaultLayout"
 
 export function Router() {
   return (
     <Routes>
-     {/* // <Route path="/" element={<DefaultLayout />}> */}
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-      {/* </Route> */}
+      </Route>
     </Routes>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,8 @@
-import { CardSmall } from "../../components/Card/CardSmall";
 
 export function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <CardSmall
-        price={99.99}
-        srcImg="src\assets\coffe-img\Type=Americano.png"
-        title="Título do produto"
-      />
     </div>
   )
 }
